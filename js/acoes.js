2bb3c09b336cb1de8b8fb9b6460ff11ba6cffcb1
LuanaTito <111540897+LuanaTito@users.noreply.github.com>
2024-03-14 22:05:43 -0300

    botão

--- js/acoes.js
+++ js/acoes.js
@@ -6,29 +6,29 @@
     var textoH2 = document.querySelector('#textos');
     //alert("Funcionou!");
 
-    
-    btazul.onclick = textoH2.textContent = 'Azul Significa Harmonia';
+btnAzul.onmouseover = () => document.body.style.backgroundColor = "blue"; 
+btnAzul.onclick = () => textoH2.textContent = 'Azul Significa Harmonia';
+btnVerde.onmouseover = () => document.body.style.backgroundColor = "green"; 
+btnVerde.onclick = () => textoH2.textContent = 'Verde Significa Saúde e Esperança';
+btnLaranja.onmouseover = () => document.body.style.backgroundColor = "orange";
+btnLaranja.onclick = () => textoH2.textContent = 'Laranja Significa Força e Energia';
 
-    btazul.onclick =document.body.classList.remove('btverde' , 'btlaranja');
-    btazul.onclick =document.body.classList.add('btazul');
-    
-    //btazul.addEventListener('click' , function(){
-       // document.className = '' //Limpar Classe (versao mais simples)
-        //document.body.classList.remove('btverde' , 'btlaranja');
-        //document.body.classList.add('btazul');
-        //textoH2.textContent = 'Azul Significa Harmonia e Maturidade';
+
+    // btazul.addEventListener('click' , function(){
+    //    // document.className = '' //Limpar Classe (versao mais simples)
+    //     document.body.classList.remove('btverde' , 'btlaranja');
+    //     document.body.classList.add('btazul');
+    //     textoH2.textContent = 'Azul Significa Harmonia e Maturidade';
         
-   // });
+    // });
     
     
-   btverde.addEventListener('click' , function(){
+    
     // btverde.addEventListener('click' , function(){
     //    // document.className = '' //Limpar Classe (versao mais simples)
     //     document.body.classList.remove('btazul' , 'btlaranja');
     //     document.body.classList.add('btverde');
-        
-        
-    //     textoH2.textContent =  'Verde Significa Saúde e Esperanca';
+    //     textoH2.textContent =  'Verde Significa Saude e Esperanca';
         
     // });
     
@@ -38,7 +38,6 @@
     //     //document.className = '' //Limpar Classe (versao mais simples)
     //     document.body.classList.remove('btverde' , 'btazul');
     //     document.body.classList.add('btlaranja');
-        
     //    textoH2.textContent = 'Laranja Significa Forca e Energia';
               
         
