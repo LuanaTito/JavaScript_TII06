d8d3b8163cf3189bb895eba861d1b96080e3d927
LuanaTito <luana.dmelo2@senacsp.edu.br>
2024-03-19 16:33:11 -0300

    funcoes JS

--- js/acoes.js
+++ js/acoes.js
@@ -6,40 +6,40 @@
     var textoH2 = document.querySelector('#textos');
     //alert("Funcionou!");
 
-    
-    btazul.onclick = textoH2.textContent = 'Azul Significa Harmonia';
+// btazul.onmouseover = () => document.body.style.backgroundColor = "blue"; 
+// btazul.onclick = () => textoH2.textContent = 'Azul Significa Harmonia';
+// btverde.onmouseover = () => document.body.style.backgroundColor = "green"; 
+// btverde.onclick = () => textoH2.textContent = 'Verde Significa Saúde e Esperança';
+// btlaranja.onmouseover = () => document.body.style.backgroundColor = "orange";
+// btlaranja.onclick = () => textoH2.textContent = 'Laranja Significa Força e Energia';
 
-    btazul.onclick =document.body.classList.remove('btverde' , 'btlaranja');
-    btazul.onclick =document.body.classList.add('btazul');
+
     
-    //btazul.addEventListener('click' , function(){
+    
+
+btazul.addEventListener('click' , function(){
        // document.className = '' //Limpar Classe (versao mais simples)
-        //document.body.classList.remove('btverde' , 'btlaranja');
-        //document.body.classList.add('btazul');
-        //textoH2.textContent = 'Azul Significa Harmonia e Maturidade';
+        document.body.classList.remove('btverde' , 'btlaranja');
+        document.body.classList.add('btazul');
+        textoH2.textContent = 'Azul Significa Harmonia e Maturidade';
         
-   // });
-    
-    
-   btverde.addEventListener('click' , function(){
-    // btverde.addEventListener('click' , function(){
-    //    // document.className = '' //Limpar Classe (versao mais simples)
-    //     document.body.classList.remove('btazul' , 'btlaranja');
-    //     document.body.classList.add('btverde');
-        
-        
-    //     textoH2.textContent =  'Verde Significa Saúde e Esperanca';
-        
-    // });
+    });
     
     
     
-    // btlaranja.addEventListener('click' , function(){
-    //     //document.className = '' //Limpar Classe (versao mais simples)
-    //     document.body.classList.remove('btverde' , 'btazul');
-    //     document.body.classList.add('btlaranja');
+    btverde.addEventListener('click' , function(){
+       // document.className = '' //Limpar Classe (versao mais simples)
+        document.body.classList.remove('btazul' , 'btlaranja');
+        document.body.classList.add('btverde');
+        textoH2.textContent =  'Verde Significa Saude e Esperanca';
         
-    //    textoH2.textContent = 'Laranja Significa Forca e Energia';
+    });
+
+    btlaranja.addEventListener('click' , function(){
+        //document.className = '' //Limpar Classe (versao mais simples)
+        document.body.classList.remove('btverde' , 'btazul');
+        document.body.classList.add('btlaranja');
+       textoH2.textContent = 'Laranja Significa Forca e Energia';
               
         
-    // });
+    });
---
+++ js/cadAcoes.js
@@ -0,0 +1,12 @@
+let nome = document.getElementById('nome')
+let idade = document.getElementById('idade')
+let salario = document.getElementById('salario')
+let cargo = document.getElementById('cargo')
+let btnsalvar = document.getElementById("btnsalvar")
+
+btnsalvar.addEventListener("click" , function (){
+    document.getElementById('fnome').innerHTML = nome.value
+    document.getElementById('fidade').innerHTML = idade.value
+    document.getElementById('fsalario').innerHTML = salario.value
+    document.getElementById('fcargo').innerHTML = cargo.value
+});
