6f354ac310e55f60bb398c9b228a2be43534118e
LuanaTito <luana.dmelo2@senacsp.edu.br>
2024-03-26 17:41:10 -0300

    26/03

--- js/aula04.js
+++ js/aula04.js
@@ -1,19 +1,20 @@
-/* function menu(){
-    let menu = document
-            .getElementById("menu");
+// function menu() {
+//     let menu = document.getElementById("menu");
+//     if (menu.style.display == "block") {
+//         menu.style.display = "none"
+//     }else{
+//     menu.style.display = "block"
+//     }
+// }
 
-    if(menu.style.display == "block")
-        menu.style.display = "none";
-    else    
-        menu.style.display = "block";
-} */
-
-let icon = document.getElementsByClassName("icon");
-let menu = document.getElementById("menu");
+let icon = document.getElementsByClassName("icon")
+let menu = document.getElementById("menu")
 
 icon[0].addEventListener('click', () => {
-    if(menu.style.display === "block")
-        menu.style.display = "none";
-    else    
-        menu.style.display = "block";
+    if (menu.style.display == "block") {
+        menu.style.display = "none"
+    } else {
+        menu.style.display = "block"
+    }
 })
+
