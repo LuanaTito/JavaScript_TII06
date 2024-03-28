0c1b2f79ce98d4c7112e0ed5c272ae8bcaf047a6
LuanaTito <111540897+LuanaTito@users.noreply.github.com>
2024-03-27 21:46:07 -0300

    Create ex_29.js

---
+++ exercicios_Js_parteII/ex_29.js
@@ -0,0 +1,10 @@
+//Exercício 2: Calcular o fatorial de um número.
+function calcularFatorial(numero) {
+    var resultado = 1;
+    for (var i = 1; i <= numero; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
+
+console.log(calcularFatorial(5)); // Saída: 120
